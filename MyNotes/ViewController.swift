4a9770b18027da5b5d9a7e76ad6c0626fef96c02
jlyons014 <jlyons014@gmail.com>
2019-11-19 09:27:39 -0500

    Initial Commit

---
+++ MyNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyNotes
+//
+//  Created by Lyons, Joseph John on 11/19/19.
+//  Copyright © 2019 Lyons, Joseph John. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
